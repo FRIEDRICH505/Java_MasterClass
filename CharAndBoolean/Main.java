38bce86d1647c4edd78b5432f39a3fc5772fc2a0
FRIEDRICH505 <67607043+FRIEDRICH505@users.noreply.github.com>
2021-01-16 19:38:39 +0200

    Add files via upload

---
+++ CharAndBoolean/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CharAndBoolean;
+
+/**
+ *
+ * @author FRIEDRICH
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+       char myChar = 'D';
+       char myUnicodeChar = '\u0044';
+       char myCopyRightChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyRightChar);
+        
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+        
+        
+    }
+}
