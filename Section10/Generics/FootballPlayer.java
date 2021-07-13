874ddd090be20442435f9b837d2b5f71f782ffb7
FRIEDRICH505 <67607043+FRIEDRICH505@users.noreply.github.com>
2021-07-13 17:46:19 +0200

    Add files via upload

---
+++ Section10/Generics/FootballPlayer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Section10.Generics;
+
+/**
+ *
+ * @author FRIEDRICH
+ */
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
